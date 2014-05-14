6b76c1f7e79be1a5a96642fd9930fc1efc2fac23
egg <djeggshell@gmail.com>
2014-05-14 10:32:13 -0700

    updated DAOs, added a jsp

--- BoxCompany/src/main/java/daos/BoxDAO.java
+++ BoxCompany/src/main/java/daos/BoxDAO.java
@@ -1,5 +1,44 @@
 package daos;
 
-public class BoxDAO {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import entities.Box;
+
+@Component
+public class BoxDAO {
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Transactional
+	public void persist(Box b){
+		
+	}
+	
+	@Transactional
+	public void update(Box b){
+		
+	}
+	
+	@Transactional
+	public void delete(Box b){
+		
+	}
+	
+	@Transactional
+	public Box getBox(){
+		
+		return null;
+	}
+	
+	@Transactional
+	public List<Box> getAllBoxes(){
+		
+		return null;
+	}
 }
--- BoxCompany/src/main/java/entities/Box.java
+++ BoxCompany/src/main/java/entities/Box.java
@@ -2,16 +2,27 @@ package entities;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
 import org.springframework.stereotype.Component;
 
 @Entity
 @Table(name = "boxes")
 @Component
 public class Box {
+	@Id
+	@GeneratedValue
+	@Column(name = "box_id")
+	private int boxId;
+	
+	@Column(name = "box_name")
 	String name;
-	List<BoxAttribute> boxAttributes;
+
+	List<BoxAttribute> boxAttributes; //will be another table linked by attribute id
 	
 	//default constructor
 	Box(){
